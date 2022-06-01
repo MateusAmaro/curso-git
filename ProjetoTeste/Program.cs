9d8e4dd8bedf9af447946a7763df734fefcdb83b
Mateus Amaro <m4teusamaro@gmail.com>
2022-06-01 20:08:01 -0300

    Acrescentada a mensagem como voce esta hoje

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -5,6 +5,7 @@ namespace ProjetoTeste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
