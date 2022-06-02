e66f400b3b544a268395f56313d703a60002664f
Mateus Amaro <m4teusamaro@gmail.com>
2022-06-01 22:57:37 -0300

    Desejando um bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -4,8 +4,8 @@ namespace ProjetoTeste {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
